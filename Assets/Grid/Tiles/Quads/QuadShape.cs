39597751eeeb9bc5469384da16718738fca366fe
daleran <daleran@gmail.com>
2016-08-30 17:42:53 -0500

    Reorganized project files

---
+++ Assets/Grid/Tiles/Quads/QuadShape.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectSpacer
+{
+
+    public enum QuadShape
+    {
+        FLAT,
+        SLOPE_DOWN,
+        SLOPE_UP,
+        CORNER,
+        CORNER_DOWN,
+        CORNER_UP,
+        WALL,
+        WALL_CORNER,
+        INVERSE_DOWN,
+        INVERSE_UP
+
+    }
+}
---
+++ Assets/Grid/Tiles/Quads/QuadTemplate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ProjectSpacer
+{
+
+    [CreateAssetMenu(fileName = "NewQuad", menuName = "Data/Templates/Quad Template", order = 1)]
+    public class QuadTemplate : ScriptableObject
+    {
+        public MeshLayer layer;
+        public QuadShape shape;
+        public Vector2Int atlasCoord;
+
+        public QuadData BuildQuad(Direction dir, bool fl)
+        {
+            return new QuadData(layer, shape, atlasCoord, dir, fl);
+        }
+
+    }
+}
